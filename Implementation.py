b302c8599e3bb9a2ca35275ac8ec19867071ee2f
JoshuaBalbi <60266737+JoshuaBalbi@users.noreply.github.com>
2022-11-21 15:22:33 -0500

    Update Implementation.py

--- Implementation.py
+++ Implementation.py
@@ -7,11 +7,12 @@ Created on Sun Nov 20 17:25:18 2022
 """
 
 import matplotlib.pyplot as plt
+import time
 import numpy as np
 import pandas as pd
 mydata1 = pd.read_csv("/Users/Joshuabalbi/Desktop/shootings.csv")
 
-#From SQL DATA
+#From SQL DATA-----------------------------------------------------------------
 state = ['CL','TX','FL','AZ','CO','GA']
 unarmed = [57,38,27,16,9,15]
 mental = [162,78,89,42,23,33]
@@ -28,67 +29,164 @@ for i in range(len(total)):
     body_cam[i] = body_cam[i]/total[i] * 100
 
 plt.bar(state,unarmed)
+plt.xlabel("States")
+plt.ylabel("percent total in state")
+plt.title("Unarmed Rates in top 6 states")
 plt.show()
 plt.bar(state,race)
+plt.xlabel("States")
+plt.ylabel("percent total in state")
+plt.title("Race Rates in top 6 states")
 plt.show()
 plt.bar(state,mental)
+plt.xlabel("States")
+plt.ylabel("percent total in state")
+plt.title("Signs of mental illness rates in top 6 states")
 plt.show()
 plt.bar(state,flee)
+plt.xlabel("States")
+plt.ylabel("percent total in state")
+plt.title("Feeling rates in top 6 states")
 plt.show()
 plt.bar(state,body_cam)
+plt.xlabel("States")
+plt.ylabel("percent total in state")
+plt.title("Inactive Body Camera rates in top 6 states")
 plt.show()
 
 
+#error-------------------------------------------------------------------------
 def laplaceMechanism(x, epsilon):
-    x +=  np.random.laplace(0, 1.0/epsilon, 1)[0]
+    x =  np.random.laplace(0, 1.0/epsilon, 1)[0]
     return x
 
-epsilon = [.1,.2,.3,.4,.5,.6,.7,.8,.9,1]
-unarmedep = np.eye(6, 10).tolist()
-mentalep = np.eye(6, 10).tolist()
-raceep = np.eye(6, 10).tolist()
-fleeep = np.eye(6, 10).tolist()
-body_camep = np.eye(6, 10).tolist()
-for i in range(len(total)):
-    for j in range(10):
-        unarmedep[i][j] = laplaceMechanism(unarmed[i], epsilon[j])
-        mentalep[i][j] = laplaceMechanism(mental[i], epsilon[j])
-        raceep[i][j] = laplaceMechanism(race[i], epsilon[j])
-        fleeep[i][j] = laplaceMechanism(flee[i], epsilon[j])
-        body_camep[i][j] = laplaceMechanism(body_cam[i], epsilon[j])
+epsilon = [.5, 1 ,1.5 ,2 ,2.5, 3]
 
-plt.bar(state,unarmedep[0])
-plt.bar(state,unarmedep[1])
-plt.bar(state,unarmedep[2])
-plt.bar(state,unarmedep[3])
-plt.bar(state,unarmedep[4])
-plt.bar(state,unarmedep[5])
+for j in range(10):
+    unarmedep = []
+    for i in range(len(total)):
+        unarmedep.append(laplaceMechanism(race[0], epsilon[i]))
+    plt.plot(epsilon,unarmedep)
+plt.xlabel("Epsilon")
+plt.ylabel("Error (%)")
+plt.title("Race in california error")
 plt.show()
-plt.bar(state,raceep[0])
-plt.bar(state,raceep[1])
-plt.bar(state,raceep[2])
-plt.bar(state,raceep[3])
-plt.bar(state,raceep[4])
-plt.bar(state,raceep[5])
+        
+for j in range(10):
+    unarmedep = []
+    for i in range(len(total)):
+        unarmedep.append(laplaceMechanism(flee[0], epsilon[i]))
+    plt.plot(epsilon,unarmedep) 
+plt.xlabel("Epsilon")
+plt.ylabel("Error (%)")
+plt.title("Fleeing rates in california error")
+plt.show()     
+  
+for j in range(10):
+    unarmedep = []
+    for i in range(len(total)):
+        unarmedep.append(laplaceMechanism(unarmed[0], epsilon[i]))
+    plt.plot(epsilon,unarmedep) 
+plt.xlabel("Epsilon")
+plt.ylabel("Error (%)")
+plt.title("Unarmed rates in california error")
 plt.show()
-plt.bar(state,mentalep[0])
-plt.bar(state,mentalep[1])
-plt.bar(state,mentalep[2])
-plt.bar(state,mentalep[3])
-plt.bar(state,mentalep[4])
-plt.bar(state,mentalep[5])
+
+for j in range(10):
+    unarmedep = []
+    for i in range(len(total)):
+        unarmedep.append(laplaceMechanism(body_cam[0], epsilon[i]))
+    plt.plot(epsilon,unarmedep) 
+plt.xlabel("Epsilon")
+plt.ylabel("Error (%)")
+plt.title("Inactive Body Cameras rates in california error")
 plt.show()
-plt.bar(state,fleeep[0])
-plt.bar(state,fleeep[1])
-plt.bar(state,fleeep[2])
-plt.bar(state,fleeep[3])
-plt.bar(state,fleeep[4])
-plt.bar(state,fleeep[5])
+
+for j in range(10):
+    unarmedep = []
+    for i in range(len(total)):
+        unarmedep.append(laplaceMechanism(mental[0], epsilon[i]))
+    plt.plot(epsilon,unarmedep) 
+plt.xlabel("Epsilon")
+plt.ylabel("Error (%)")
+plt.title("Signs of mental Illness rates in california error")
 plt.show()
-plt.bar(state,body_camep[0])
-plt.bar(state,body_camep[1])
-plt.bar(state,body_camep[2])
-plt.bar(state,body_camep[3])
-plt.bar(state,body_camep[4])
-plt.bar(state,body_camep[5])
+
+#runtime-----------------------------------------------------------------------
+
+for j in range(10):
+    unarmedep = []
+    runtime = []
+    for i in range(len(total)):
+        start_time = time.time()
+        unarmedep.append(laplaceMechanism(race[0], epsilon[i]))
+        runtime.append(time.time() - start_time)
+    plt.plot(epsilon,runtime)
+plt.ylim(0,.00007)
+plt.xlabel("Epsilon")
+plt.ylabel("Runtime (S)")
+plt.title("Race in california error")
+plt.show()
+
+
+for j in range(10):
+    unarmedep = []
+    runtime = []
+    for i in range(len(total)):
+        start_time = time.time()
+        unarmedep.append(laplaceMechanism(flee[0], epsilon[i]))
+        runtime.append(time.time() - start_time)
+    plt.plot(epsilon,runtime)
+plt.ylim(0,.00007)
+plt.xlabel("Epsilon")
+plt.ylabel("Runtime (S)")
+plt.title("Fleeing rates in california error")
+plt.show()    
+
+
+for j in range(10):
+    unarmedep = []
+    runtime = []
+    for i in range(len(total)):
+        start_time = time.time()
+        unarmedep.append(laplaceMechanism(unarmed[0], epsilon[i]))
+        runtime.append(time.time() - start_time)
+    plt.plot(epsilon,runtime)
+plt.ylim(0,.00007)
+plt.xlabel("Epsilon")
+plt.ylabel("Runtime (S)")
+plt.title("Unarmed rates in california error")
+plt.show()
+
+
+
+for j in range(10):
+    unarmedep = []
+    runtime = []
+    for i in range(len(total)):
+        start_time = time.time()
+        unarmedep.append(laplaceMechanism(body_cam[0], epsilon[i]))
+        runtime.append(time.time() - start_time)
+    plt.plot(epsilon,runtime)
+plt.ylim(0,.00007)
+plt.xlabel("Epsilon")
+plt.ylabel("Runtime (S)")
+plt.title("Inactive Body Cameras rates in california error")
+plt.show()
+
+
+
+
+for j in range(10):
+    unarmedep = []
+    runtime = []
+    for i in range(len(total)):
+        start_time = time.time()
+        unarmedep.append(laplaceMechanism(mental[0], epsilon[i]))
+        runtime.append(time.time() - start_time)
+    plt.plot(epsilon,runtime)
+plt.ylim(0,.00007)
+plt.xlabel("Epsilon")
+plt.ylabel("Runtime (S)")
+plt.title("Signs of mental Illness rates in california error")
 plt.show()
